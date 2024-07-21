5be286d1293ed4228802083882478e935a4e7698
Beiri Alife <beiri.al@outlook.com>
2024-07-21 02:39:06 -0300

    Remove old stylesheet import for trix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,41 +13,67 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/0.11.1/trix.css">
 
     <!-- Scripts -->
     <script>
         window.App = {!! json_encode([
             'csrfToken' => csrf_token(),
             'user' => Auth::user(),
-            'signedIn' => Auth::check()
+            'signedIn' => Auth::check(),
         ]) !!};
     </script>
 
     <style>
-        body { padding-bottom: 100px; }
-        .level { display: flex; align-items: center; }
-        .level-item { margin-right: 1em; }
-        .flex { flex: 1; }
-        .mr-1 { margin-right: 1em; }
-        .ml-a { margin-left: auto; }
-        [v-cloak] { display: none; }
-        .ais-highlight > em { background: yellow; font-style: normal; }
+        body {
+            padding-bottom: 100px;
+        }
+
+        .level {
+            display: flex;
+            align-items: center;
+        }
+
+        .level-item {
+            margin-right: 1em;
+        }
+
+        .flex {
+            flex: 1;
+        }
+
+        .mr-1 {
+            margin-right: 1em;
+        }
+
+        .ml-a {
+            margin-left: auto;
+        }
+
+        [v-cloak] {
+            display: none;
+        }
+
+        .ais-highlight>em {
+            background: yellow;
+            font-style: normal;
+        }
     </style>
 
     @yield('head')
 </head>
+
 <body>
-<div id="app">
-    @include ('layouts.nav')
+    <div id="app">
+        @include ('layouts.nav')
 
-    @yield('content')
+        @yield('content')
 
-    <flash message="{{ session('flash') }}"></flash>
-</div>
+        <flash message="{{ session('flash') }}"></flash>
+    </div>
 
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
-@yield('scripts')
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
+
 </html>
