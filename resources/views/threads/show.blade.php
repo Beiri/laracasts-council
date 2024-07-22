87f351ca6e32e48c4edcf3930f85b59c29fd1ee9
Beiri Alife <beiri.al@outlook.com>
2024-07-22 16:20:03 -0300

    Add link to thread profile creator

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,19 +19,18 @@
                         <div class="panel-body">
                             <p>
                                 This thread was published {{ $thread->created_at->diffForHumans() }} by
-                                <a href="#">{{ $thread->creator->name }}</a>, and currently
-                                has <span
-                                        v-text="repliesCount"></span> {{ str_plural('comment', $thread->replies_count) }}
+                                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>, and
+                                currently
+                                has <span v-text="repliesCount"></span> {{ str_plural('comment', $thread->replies_count) }}
                                 .
                             </p>
 
                             <p>
-                                <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}" v-if="signedIn"></subscribe-button>
+                                <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}"
+                                    v-if="signedIn"></subscribe-button>
 
-                                <button class="btn btn-default"
-                                        v-if="authorize('isAdmin')"
-                                        @click="toggleLock"
-                                        v-text="locked ? 'Unlock' : 'Lock'"></button>
+                                <button class="btn btn-default" v-if="authorize('isAdmin')" @click="toggleLock"
+                                    v-text="locked ? 'Unlock' : 'Lock'"></button>
                             </p>
                         </div>
                     </div>
