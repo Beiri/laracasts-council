11860c1360287fe4bd76b9b7c475b0dadf77ced1
Beiri Alife <beiri.al@outlook.com>
2024-07-22 17:13:24 -0300

    Delete old avatar

--- app/Http/Controllers/Api/UserAvatarController.php
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class UserAvatarController extends Controller
 {
@@ -13,11 +14,15 @@ class UserAvatarController extends Controller
      */
     public function store()
     {
+        /** @var \App\User $user */
+        $user = auth()->user();
         request()->validate([
             'avatar' => ['required', 'image']
         ]);
 
-        auth()->user()->update([
+        Storage::disk('public')->delete($user->getOriginal('avatar_path'));
+
+        $user->update([
             'avatar_path' => request()->file('avatar')->store('avatars', 'public')
         ]);
 
