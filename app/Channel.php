7fb5fed73f2627861e263c5c8825110c73afff4f
Beiri Alife <beiri.al@outlook.com>
2024-07-23 00:28:21 -0300

    implement named method for including archived chanels

--- app/Channel.php
+++ app/Channel.php
@@ -18,6 +18,9 @@ class Channel extends Model
         'archived' => 'boolean',
     ];
 
+    /**
+     * Boot the channels model.
+     */
     protected static function boot()
     {
         parent::boot();
@@ -65,5 +68,13 @@ class Channel extends Model
     {
         $this->attributes['name'] = $name;
         $this->attributes['slug'] = str_slug($name);
+    }
+
+    /**
+     * Get a new query builder that includes archives.
+     */
+    public static function withArchived()
+    {
+        return (new static)->newQueryWithoutScope('active');
     }
 }
