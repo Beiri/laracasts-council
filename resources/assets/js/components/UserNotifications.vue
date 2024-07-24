21a1b1ac76304c86a484da5339be40d3409312b8
Beiri Alife <beiri.al@outlook.com>
2024-07-24 01:44:51 -0300

    Fix for notifications not being marked as read

--- resources/assets/js/components/UserNotifications.vue
+++ resources/assets/js/components/UserNotifications.vue
@@ -1,35 +1,50 @@
 <template>
-    <li class="dropdown" v-if="notifications.length">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <span class="glyphicon glyphicon-bell"></span>
-        </a>
+  <li class="dropdown" v-if="notifications.length">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+      <span class="glyphicon glyphicon-bell"></span>
+    </a>
 
-        <ul class="dropdown-menu">
-            <li v-for="notification in notifications">
-                <a :href="notification.data.link"
-                   v-text="notification.data.message"
-                   @click="markAsRead(notification)"
-                ></a>
-            </li>
-        </ul>
-    </li>
+    <ul class="dropdown-menu">
+      <li v-for="notification in notifications" :key="notification.id">
+        <a
+          :href="notification.data.link"
+          v-text="notification.data.message"
+          @click.prevent="markAsRead(notification)"
+        ></a>
+      </li>
+    </ul>
+  </li>
 </template>
 
 <script>
-    export default {
-        data() {
-            return { notifications: false }
-        },
+export default {
+  data() {
+    return { notifications: false };
+  },
 
-        created() {
-            axios.get('/profiles/' + window.App.user.name + '/notifications')
-                .then(response => this.notifications = response.data);
-        },
+  created() {
+    this.fetchNotifications();
+  },
 
-        methods: {
-            markAsRead(notification) {
-                axios.delete('/profiles/' + window.App.user.name + '/notifications/' + notification.id)
-            }
-        }
-    }
+  computed: {
+    endpoint() {
+      return `/profiles/${window.App.user.name}/notifications`;
+    },
+  },
+
+  methods: {
+    fetchNotifications() {
+      axios
+        .get(this.endpoint)
+        .then((response) => (this.notifications = response.data));
+    },
+
+    markAsRead(notification) {
+      axios.delete(`${this.endpoint}/${notification.id}`).then(({ data }) => {
+        this.fetchNotifications();
+        document.location.replace(data.link);
+      });
+    },
+  },
+};
 </script>
