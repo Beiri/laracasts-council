cea9223d56501fe32b2af2620641a9792d9d5c99
Beiri Alife <beiri.al@outlook.com>
2024-07-25 00:32:54 -0300

    Wrap the Modals in a div with the v-cloak to prevent the momentary "blink"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,9 @@
 
         <flash message="{{ session('flash') }}"></flash>
 
-        @include('modals.all')
+        <div v-cloak>
+            @include('modals.all')
+        </div>
     </div>
 
     <!-- Scripts -->
