cb53c6ce4cf4ab0753250c0be24742b611833cd0
Beiri Alife <beiri.al@outlook.com>
2024-07-25 02:19:36 -0300

    Return RefreshDatabase

--- tests/Feature/BestReplyTest.php
+++ tests/Feature/BestReplyTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class BestReplyTest extends TestCase
 {
-    use DatabaseMigrations;
+    use RefreshDatabase;
 
     /** @test */
     public function a_thread_creator_may_mark_any_reply_as_the_best_reply()
