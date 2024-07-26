67384cc9ca32a42b6d401950ac86098367aa9cc5
Beiri Alife <beiri.al@outlook.com>
2024-07-26 19:40:02 -0300

    Consolidate to single feed() method

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -31,7 +31,7 @@ class ActivityTest extends TestCase
     }
 
     /** @test */
-    function it_records_activity_when_a_reply_is_created()
+    public function it_records_activity_when_a_reply_is_created()
     {
         $this->signIn();
 
@@ -41,24 +41,19 @@ class ActivityTest extends TestCase
     }
 
     /** @test */
-    function it_fetches_a_feed_for_any_user()
+    public function it_fetches_a_feed_for_any_user()
     {
         $this->signIn();
         /** @var \App\User $user */
         $user = auth()->user();
 
-        create(\App\Thread::class, ['user_id' => auth()->id()], 2);
+        create(\App\Thread::class, ['user_id' => auth()->id()], 3);
 
         $user->activity()->first()->update(['created_at' => Carbon::now()->subWeek()]);
 
-        $feed = Activity::feed($user, 50);
+        $feed = Activity::feed($user);
 
-        $this->assertTrue($feed->keys()->contains(
-            Carbon::now()->format('Y-m-d')
-        ));
-
-        $this->assertTrue($feed->keys()->contains(
-            Carbon::now()->subWeek()->format('Y-m-d')
-        ));
+        $this->assertCount(3, $feed->all());
+        $this->assertEquals([1, 1, 1], $feed->pluck('user_id')->toArray());
     }
 }
