7a7b16455b787ee6f3c3d91b7f33347c71c512e9
Beiri Alife <beiri.al@outlook.com>
2024-07-26 20:30:07 -0300

    Use the redirectTo property of the LoginController, to decide where to redirect after a successful login.

--- resources/assets/js/components/Login.vue
+++ resources/assets/js/components/Login.vue
@@ -11,8 +11,15 @@ export default {
     login() {
       this.loading = true;
       axios
-        .post("/login", this.form)
-        .then(() => location.reload())
+        .post("/login", this.form, {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        })
+        .then(({ data: { redirect } }) => {
+          location.assign(redirect);
+        })
         .catch((error) => {
           this.feedback =
             "The given credentials are incorrect. Please try again.";
